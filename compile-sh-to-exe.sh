3b1252de781f8450d89080d8973e94d2c80b7295
机电匠 <77825661+JiDianJiang@users.noreply.github.com>
2023-05-03 18:05:11 +0800

    把sh编译成exe ( 已测 ) 2023.05.03 18:05

--- compile-sh-to-exe.sh
+++ compile-sh-to-exe.sh
@@ -5,8 +5,7 @@
 # 把shell脚本编译成可执行文件
 
 # 获取脚本命令行第一个参数 并检查是否存在
-VAR_1=$1
-if [ -z "$VAR_1" ]; then echo "Please enter the name of the file to be compiled."; echo "Example: $0 /a/b/c/x.sh"; exit 1; fi
+VAR_1=$1; if [ -z "$VAR_1" ]; then echo "Please enter the name of the file to be compiled."; echo "Example: $0 /a/b/c/x.sh"; exit 1; fi
 
 # 检查脚本命令行第一个参数 输入的文件是否存在
 if [ ! -f "$VAR_1" ]; then echo "Error: file does not exist."; exit 1; fi
@@ -20,18 +19,17 @@ compile-sh-to-exe() {
     if [ ! -f "$1" ]; then echo "Error: script file does not exist."; return 1; fi
     # 获取脚本文件路径和名称
     local script_file=$(basename "$1"); local script_dir=$(dirname "$1")
-    local script_path=$(cd "$script_dir" && pwd)/"$script_file"
+    local script_path=$(cd "$script_dir" && pwd)/"$script_file"; local cmd=""
     # 使用shc编译
-    local shc_cmd="shc -f $script_path"
-    if ! $shc_cmd; then return 1; fi
-    local rm_cmd="rm ${script_dir}/${script_file}.x"
-    if ! $rm_cmd; then return 1; fi
+    cmd="shc -f $script_path"; if ! $cmd; then return 1; fi
+    cmd="rm ${script_dir}/${script_file}.x"; if ! $cmd; then return 1; fi
     # 使用gcc编译
-    local gcc_cmd="gcc -static ${script_dir}/${script_file}.x.c -o ${script_dir}/${script_file}.gx"
-    if ! $gcc_cmd; then return 1; fi
-    local rm_cmd="rm ${script_dir}/${script_file}.x.c"
-    if ! $rm_cmd; then return 1; fi
-    ldd ${script_dir}/${script_file}.gx
+    cmd="gcc -static ${script_dir}/${script_file}.x.c -o ${script_dir}/${script_file}.gx"
+    if ! $cmd; then 
+        cmd="gcc ${script_dir}/${script_file}.x.c -o ${script_dir}/${script_file}.gx"; if ! $cmd; then return 1; fi
+    fi
+    cmd="rm ${script_dir}/${script_file}.x.c"; if ! $cmd; then return 1; fi
+    echo; ldd ${script_dir}/${script_file}.gx; echo
     # 编译成功
     return 0
 }
